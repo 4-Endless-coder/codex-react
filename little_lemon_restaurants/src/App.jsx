a7992b52702bae758b354902b2ca120152ad6c77
4-Endless-coder <asheshdash4@gmail.com>
2025-09-03 14:43:22 +0530

    Setting up the app components

--- little_lemon_restaurants/src/App.jsx
+++ little_lemon_restaurants/src/App.jsx
@@ -1,22 +1,33 @@
-import About from "./components/About";
-import Footer from "./components/Footer";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Testimonial from "./components/Testimonial";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main>
-        <Hero />
-        <Highlights />
-        <Testimonial />
-        <About />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Highlights />
+                <Testimonial />
+                <About />
+              </>
+            }
+          />
+        </Routes>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
--- little_lemon_restaurants/src/components/Nav.jsx
+++ little_lemon_restaurants/src/components/Nav.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <>
-    <nav>
-      <img src="src\assets\icons_assets\Logo.svg" alt="Little Lemon Logo" />
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/reservations">Reservations</a></li>
-        <li><a href="/order">Order Online</a></li>
-        <li><a href="/login">Login</a></li>
-      </ul>
-    </nav>
+      <nav>
+        <img src="src\assets\icons_assets\Logo.svg" alt="Little Lemon Logo" />
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/menu">Menu</Link>
+          </li>
+          <li>
+            <Link to="/reservations">Reservations</Link>
+          </li>
+          <li>
+            <Link to="/order">Order Online</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
